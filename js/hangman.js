4f269593440ebacd2e06f41fb1650058053539e1
MartinAhlsen <martin.ahlsen@gmail.com>
2022-12-02 18:21:53 +0100

    cancel button

--- js/hangman.js
+++ js/hangman.js
@@ -15,9 +15,9 @@ const ALLOWED_CHARACTERS = ["A", "B", "C", "D", "E", "F", "G", "H",
 let displayedCharacters = Array(randomAnswer.length).fill(" _ ");       // Creates an array with "_" equal to length of answer
 console.log("------------------");
 console.log("The name is " + randomAnswer.length + " characters long");
-alert("The name is " + randomAnswer.length + " characters long");
+alert("The name you are looking for is " + randomAnswer.length + " characters long");
 console.log("When you guess correctly, you will see your letter displayed below");
-alert("When you guess correctly, you will see your letter displayed below");
+alert("Your porgress will be displayed in the console");
 
 
 let playerGuesses = [];     // Array of cumulative player inputs
@@ -26,6 +26,51 @@ let numberOfRounds = 1;     // Number of rounds counter
 
 
 
+
+while (1 === 1) {       // !!! -= The game loop =- !!!
+    
+    console.log("------------------");
+    console.log("This is round number " + numberOfRounds);
+    
+    
+    let characterOfChoice = prompt("Choose a single letter from A to Z");       // Input
+    
+    if (characterOfChoice === null) {
+        alert("Refresh page to play again")
+        break
+    }
+    
+    let characterOfChoiceCapitalized = characterOfChoice.toUpperCase();     // Converts input to uppercase
+    
+    if (testChosenCharacter(characterOfChoiceCapitalized) === false) {      // Is input character an allowed letter?
+        alert("Faulty input, try again");
+        continue;
+    }
+    
+    doesCharacterMatch(characterOfChoiceCapitalized);       // Does input match a letter of the answer
+    
+    console.log(" ");       // Logging information for the user
+    console.log("Your guesses so far in alphabetical order:");
+    console.log(playerGuesses.sort().join(" "));
+    console.log(" ");
+    console.log("Your progress:")
+    console.log(displayedCharacters.join(" "));
+    console.log(" ");
+    
+    if (didYouWin(displayedCharacters, randomAnswer) === false) {       // Check win condition
+        numberOfRounds++;
+        continue;
+    } else {
+        console.log("You won in " + playerGuesses.length + " guesses!");
+        alert("You won in " + playerGuesses.length + " guesses!");
+        numberOfRounds = 0;
+        break;
+    }
+}
+
+
+
+//Functions
 function testChosenCharacter(letter) {
     for (let i = 0; i < ALLOWED_CHARACTERS.length; i++) {       // Tests if input character is allowed
         if (letter === ALLOWED_CHARACTERS[i]) {
@@ -58,41 +103,3 @@ function didYouWin (displayedCharacters, randomAnswer) {        // Checks win co
     }
     return true;
 }
-
-while (1 === 1) {       // !!! -= The game loop =- !!!
-
-    console.log("------------------");
-    console.log("This is round number " + numberOfRounds);
-
-
-    let characterOfChoice = prompt("Choose a single letter from A to Z");       // Input
-    let characterOfChoiceCapitalized = characterOfChoice.toUpperCase();     // Converts input to uppercase
-    
-    if (testChosenCharacter(characterOfChoiceCapitalized) === false) {      // Is input character an allowed letter?
-        alert("Faulty input, try again");
-        continue;
-    }
-
-    doesCharacterMatch(characterOfChoiceCapitalized);       // Does input match a letter of the answer
-
-    console.log(" ");
-    console.log("Your guesses so far in alphabetical order:");      // Logging information for the user
-    console.log(playerGuesses.sort().join(" "));
-    console.log(" ");
-    console.log("Your progress:")
-    console.log(displayedCharacters.join(" "));
-    console.log(" ");
-
-    if (didYouWin(displayedCharacters, randomAnswer) === false) {       // Check win condition
-        numberOfRounds++;
-        continue;
-    } else {
-        numberOfRounds = 0;
-        break;
-    }
-}
-
-console.log("You won in " + playerGuesses.length + " guesses!");
-alert("You won in " + playerGuesses.length + " guesses!");
-
-
